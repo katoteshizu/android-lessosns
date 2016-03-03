56907829859173e72828efc4c73832c3c47fb46f
Andrew Matsur <matsurator@ya.ru>
2016-03-03 11:00:36 +0300

    multiple screen

---
+++ p1151multiplescreen/src/main/java/ru/startandroid/p1151multiplescreen/DetailsActivity.java
@@ -0,0 +1,36 @@
+package ru.startandroid.p1151multiplescreen;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @authored by Andrew on 3/3/2016.
+ */
+public class DetailsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE
+                && isLarge()) {
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null) {
+            DetailsFragment details = DetailsFragment.newInstance(getIntent()
+                    .getIntExtra("position", 0));
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, details).commit();
+        }
+    }
+
+    boolean isLarge() {
+        return (getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+}
